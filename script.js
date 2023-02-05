b62924da54f1683781c9fb866f6d2428e11da0d8
santunu <santanuroy253396@gmail.com>
2023-02-05 23:48:47 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+var myModal = document.getElementById("accio-modal")
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
